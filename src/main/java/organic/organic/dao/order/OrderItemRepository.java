17e5d5cdcb3244a605c616cd6692089ed7a54365
huphc1024 <leehuphc@gmail.com>
2018-12-24 04:09:06 +0700

    hhuhu

--- src/main/java/organic/organic/dao/order/OrderItemRepository.java
+++ src/main/java/organic/organic/dao/order/OrderItemRepository.java
@@ -1,8 +1,14 @@
 package organic.organic.dao.order;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import organic.organic.model.orders.OrdersItem;
 
-public interface OrderItemRepository extends JpaRepository<OrdersItem, Integer> {
+import java.util.List;
 
+public interface OrderItemRepository extends JpaRepository<OrdersItem, Integer> {
+    @Query( value = "DELETE FROM tbl_order_item WHERE tbl_order_id = ?1",  nativeQuery = true)
+    public int deleteALlByOrderId(int id);
+    @Query( value = "SELECT id FROM tbl_order_item WHERE tbl_order_id = ?1",  nativeQuery = true)
+    public List<Integer> listOrdersItemByOrder(int id_order);
 }
--- src/main/java/organic/organic/dao/order/OrderRepository.java
+++ src/main/java/organic/organic/dao/order/OrderRepository.java
@@ -8,4 +8,6 @@ import organic.organic.model.orders.RefOrderStatus;
 public interface OrderRepository extends JpaRepository<Orders, Integer> {
     @Query( value = "SELECT r.id FROM tbl_order r WHERE r.tbl_user_id = ?1 ORDER BY r.id DESC LIMIT 1",  nativeQuery = true)
     public int findOrderByIdUser(int id);
+    @Query( value = "update tbl_order set ref_order_status_code = ?1 where id = ?2",  nativeQuery = true)
+    public int updateStatusOrder(String status, int id);
 }
